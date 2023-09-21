5ce4c41df205c2c363710d89c1b394df5ce853f0
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-21 23:09:53 +0100

    Add functionality to play audio repeatedly

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('keydown', function(e){
     const audio = document.querySelector(`audio[data-key="${(e.key).toUpperCase()}"]`)
-    console.log(audio)
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
 });
