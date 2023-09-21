5016433f339a49a9b855b2e2ffca98f014f57c6d
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-21 23:06:08 +0100

    Fix toUpperCase() function

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,4 +1,4 @@
 window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${(e.key).toUpperCase}"]`)
+    const audio = document.querySelector(`audio[data-key="${(e.key).toUpperCase()}"]`)
     console.log(audio)
 });
